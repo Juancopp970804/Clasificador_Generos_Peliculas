3fff2fb16866093375e5d36589d3c33db9b4a7cb
JuanCamiloPerezPEna <juancamiloperezpena@gmail.com>
2022-05-29 17:34:00 -0500

    v2

--- api.py
+++ api.py
@@ -64,4 +64,4 @@ class PhishingApi(Resource):
     
     
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=4444)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
