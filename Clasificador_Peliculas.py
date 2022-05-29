5d609a45b8a3ec8f871c77f1b187979cca5abb06
JuanCamiloPerezPEna <juancamiloperezpena@gmail.com>
2022-05-29 15:27:50 -0500

    v1

---
+++ Clasificador_Peliculas.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from gensim.models import KeyedVectors
+from tensorflow.keras.models import load_model
+from tensorflow.keras.backend import clear_session
+import tensorflow as tf
+import numpy as np
+
+
+def plot_representation(document_i, TL_Words_vectors):
+    # TL_Words_vectors = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin", binary=True)
+    # print(1)
+
+    avg_vector = 0
+    for word in (document_i.lower().split()):
+        try:
+            avg_vector += TL_Words_vectors[word]
+        except:
+            continue
+    avg_vector = avg_vector/len(document_i.lower().split())
+    return avg_vector
+
+def generar_Clasificacion(plot_prueba, TL_Words_vectors):
+    vector_plot = pd.DataFrame()
+    vector_plot[0] = plot_representation(plot_prueba, TL_Words_vectors)
+    vector_plot  = np.transpose(vector_plot)
+    print(vector_plot)
+    
+
+    cols = ['p_Action', 'p_Adventure', 'p_Animation', 'p_Biography', 'p_Comedy', 'p_Crime', 'p_Documentary', 'p_Drama', 'p_Family',
+            'p_Fantasy', 'p_Film-Noir', 'p_History', 'p_Horror', 'p_Music', 'p_Musical', 'p_Mystery', 'p_News', 'p_Romance',
+            'p_Sci-Fi', 'p_Short', 'p_Sport', 'p_Thriller', 'p_War', 'p_Western']
+
+
+    dicc_salida = {}
+    categorias_finales = []
+    for i, categoria in enumerate(cols):
+        try:
+            del mod_load
+        except:
+            pass
+            
+        mod_load = load_model('modelos/red_' + categoria + '.h5')
+        y_pred_genres = mod_load.predict(vector_plot)
+        
+        dicc_salida[categoria] = y_pred_genres[0][0]
+
+        if y_pred_genres[0][0] >= 0.5:
+            categorias_finales.append(categoria)
+
+        if i == 0:
+            y_pred_total = y_pred_genres
+        else:
+            y_pred_total = np.concatenate([y_pred_total,y_pred_genres], axis= 1)
+
+    # print(dicc_salida)
+
+
+    return dicc_salida, categorias_finales
+
+
+# plot_prueba_1 = '''
+# major benson winifred payne is being discharged from the marines .  payne is a killin '  machine ,  but the wars of the world are no longer fought on the battlefield .  a career marine ,  he has no idea what to do as a civilian ,  so his commander finds him a job  -  commanding officer of a local school ' s jrotc program ,  a bunch or ragtag losers with no hope .  using such teaching tools as live grenades and real bullets ,  payne starts to instill the corp with some hope .  but when payne is recalled to fight in bosnia ,  will he leave the corp that has just started to believe in him ,  or will he find out that killin '  ain ' t much of a livin '  ?
+# '''
+
+# print(generar_Clasificacion(plot_prueba_1))
---
+++ api.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restx import Api, Resource, fields
+import joblib
+from gensim.models import KeyedVectors
+from Clasificador_Peliculas import generar_Clasificacion
+
+TL_Words_vectors = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin", binary=True)
+
+app = Flask(__name__)
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='Prediccion de los generos de una pelicula',
+    description='API para la prediccion de los generos de una pelicula')
+
+ns = api.namespace('Clasificar',
+     description='Generos de una pelicula')
+   
+parser = api.parser()
+
+parser.add_argument(
+    'year',
+    type=int,
+    required=True, 
+    help='Ano de la pelicula',
+    location='args')
+
+parser.add_argument(
+    'title',
+    type=str,
+    required=True,
+    help='Titulo de la pelicula',
+    location='args')
+
+parser.add_argument(
+    'plot',
+    type=str,
+    required=True,
+    help='Descripcion o resumen de la pelicula',
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'Probabilidades': fields.String,
+    'Categorias': fields.String,
+})
+
+@ns.route('/')
+class PhishingApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        print(args)
+        probs, categ = generar_Clasificacion(args['plot'],TL_Words_vectors)
+        return {
+                #    "result": 1
+        #  "result": generar_Clasificacion(args['plot'],TL_Words_vectors)
+        "Probabilidades": probs,
+        "Categorias": categ
+        }, 200
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=4444)
